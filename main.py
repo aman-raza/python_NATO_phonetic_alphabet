2508fcad099fb72cb137f31aba268b91c0e5399f
aman <53443872+aman-raza@users.noreply.github.com>
2024-02-05 14:39:05 +0530

    Update main.py
    implemented a try-catch block

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ data_frame = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 phonetic_dictionary = {row.letter: row.code for (index, row) in data_frame.iterrows()}
 
-word = input("Enter a word : ").upper()
+def generate_phonetics():
+    word = input("Enter a word : ").upper()
+    try:
+        my_list = [phonetic_dictionary[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetics()
+    else:
+        print(my_list)
 
-my_list = [phonetic_dictionary[letter] for letter in word]
-
-print(my_list)
+generate_phonetics()
